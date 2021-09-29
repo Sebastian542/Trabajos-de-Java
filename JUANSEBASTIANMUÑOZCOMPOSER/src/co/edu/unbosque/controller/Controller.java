3ab108fae80bfbf31bf5c05e22876a810cb9f7e7
SebastianMp322 <juansebas_2001@hotmail.com>
2021-09-29 12:03:18 -0500

    V1

---
+++ JUANSEBASTIANMUÑOZCOMPOSER/src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,34 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.model.Crear_cancion;
+import co.edu.unbosque.model.persistence.EjemploFile;
+import co.edu.unbosque.view.View;
+
+public class Controller {
+	
+	private Crear_cancion c;
+
+	private View gui;
+	
+	private EjemploFile archivo;
+	
+	public Controller() {
+		
+		
+		gui = new View();
+		
+		c=new Crear_cancion();
+		
+		archivo=new EjemploFile();
+
+		funcionar();
+	}
+
+	private void funcionar() {
+		
+	
+		gui.mostrarResultados(c.generarCancion());
+
+		
+	}
+}
---
+++ JUANSEBASTIANMUÑOZCOMPOSER/src/co/edu/unbosque/model/Crear_cancion.java
@@ -0,0 +1,67 @@
+package co.edu.unbosque.model;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.JFileChooser;
+
+import co.edu.unbosque.model.persistence.EjemploFile;
+
+public class Crear_cancion {
+
+
+
+	
+	
+	public String generarCancion() {
+		
+		
+		EjemploFile archivo=new EjemploFile();
+	
+		String estrofa1=generarEstrofa();
+		
+
+		String estrofa2=generarEstrofa();
+		
+		String estrofa3=generarEstrofa();
+		
+		archivo.escribirArchivo("La estrofa 1 es \n"+estrofa1+"\nLa estrofa 2 es \n"+estrofa2+"\nLa estrofa 3 es \n"+estrofa3);
+		
+		return "La estrofa 1 es \n"+estrofa1+"\n La estrofa 2 es \n"+estrofa2+"\n La estrofa 3 es \n"+estrofa3;		
+
+	}
+	
+	
+	
+	public String generarEstrofa() {
+		
+		String[ ] palabras1 = {"Mami ", "Bebe ","Princess ", "Mami "};   
+		String[ ] palabras2 = {"yo quiero ", "yo puedo ","yo vengo a ", "voy a "};   
+		String[ ] palabras3 = {"enceldete ", "amalte ","ligalte ", "fugalte "};   
+		String[ ] palabras4 = {"suave ", "lento ","rapido ", "fuerte "};   
+		String[ ] palabras5 = {"hasta que salga el sol  ", "toda la noche ","hasta el amanecer ", "todo el dia "};   
+		String[ ] palabras6 = {"sin anestecia ", "complomiso ","feis to feis ", "sin miedo "};
+	
+		String renglon1=palabras1[generarAleatorio()]+palabras2[generarAleatorio()]+palabras3[generarAleatorio()]+palabras4[generarAleatorio()]+palabras5[generarAleatorio()]+palabras6[generarAleatorio()];
+		String renglon2=palabras1[generarAleatorio()]+palabras2[generarAleatorio()]+palabras3[generarAleatorio()]+palabras4[generarAleatorio()]+palabras5[generarAleatorio()]+palabras6[generarAleatorio()];
+		String renglon3=palabras1[generarAleatorio()]+palabras2[generarAleatorio()]+palabras3[generarAleatorio()]+palabras4[generarAleatorio()]+palabras5[generarAleatorio()]+palabras6[generarAleatorio()];
+		String renglon4=palabras1[generarAleatorio()]+palabras2[generarAleatorio()]+palabras3[generarAleatorio()]+palabras4[generarAleatorio()]+palabras5[generarAleatorio()]+palabras6[generarAleatorio()];
+
+		
+		
+		return renglon1+"\n"+renglon2+"\n"+renglon3+"\n"+renglon4;
+
+	}
+	
+
+	public int generarAleatorio() {
+		
+		return (int) (Math.random() * (3- 0) + 0);
+	}
+	
+
+	
+}
