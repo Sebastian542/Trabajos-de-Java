98ea28cf74ecf387172c4ded38edc58884918e38
Juan Sebastian Muñoz Peñuela <juansebas_2001@hotmail.com>
2021-10-29 12:35:28 -0500

    Panel2

--- EcorpJuegos/src/co/edu/unbosque/controller/Controller.java
+++ EcorpJuegos/src/co/edu/unbosque/controller/Controller.java
@@ -1,15 +1,17 @@
 package co.edu.unbosque.controller;
 
-package co.edu.unbosque.controller;
+
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import co.edu.unbosque.model.ClaseX;
+import co.edu.unbosque.view.Panel2;
 import co.edu.unbosque.view.View;
 
 public class Controller implements ActionListener{
 
+	private Panel2 p2;
 	private ClaseX m;
 	private View gui;
 	
@@ -18,15 +20,29 @@ public class Controller implements ActionListener{
 		gui = new View(this);
 
 		gui.setVisible(true);
+		funcionar();
+		
 	}
+	
+	public void funcionar() {
+		
+		
+
+	}
+	
+
+	
 	
 	@Override
 	public void actionPerformed(ActionEvent evento) {
 		// TODO Auto-generated method stub
-		if (evento.getActionCommand().equals(gui.getPanelEntrada().CONVERTIR)) {
+		
+		
+		
+		//if (evento.getActionCommand().equals(gui.getPanelEntrada().CONVERTIR)) {
 			//String resultado = m.procesarDato(gui.getPanelEntrada().getTxtNumero().getText());
-			String resultado1 = m.getY().procesarDatoClaseY(gui.getPanelEntrada().getTxtNumero().getText());
-			gui.getPanelResultados().getTxtMonto().setText(resultado1);
-		}
+			//String resultado1 = m.getY().procesarDatoClaseY(gui.getPanelEntrada().getTxtNumero().getText());
+			//gui.getPanelResultados().getTxtMonto().setText(resultado1);
+		//}
 	}
 }
--- EcorpJuegos/src/co/edu/unbosque/view/Panel2.java
+++ EcorpJuegos/src/co/edu/unbosque/view/Panel2.java
@@ -1,16 +1,67 @@
 package co.edu.unbosque.view;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+import java.awt.Color;
 import java.awt.GridLayout;
+import javax.swing.JList;
 
 public class Panel2 extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
+	private static final long serialVersionUID = 1L;
+	private JLabel nombreJugador1;
+	public static final String CONVERTIR = "Convertir";
+	private JLabel lblNewLabel;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JLabel lblNewLabel_1;
+	private JButton btnNewButton;
+	private JList list;
+	
 	public Panel2() {
-		setLayout(new GridLayout(1, 0, 0, 0));
+		setLayout( new GridLayout(2,2) );//se define el grid de 2x2
+		TitledBorder border = BorderFactory.createTitledBorder("Datos de Entrada");
+		border.setTitleColor(Color.BLACK);
+		setBorder( border );
+		nombreJugador1 = new JLabel("El nombre del jugador 1 es :");
+		add(nombreJugador1);
+		
+		textField = new JTextField();
+		add(textField);
+		textField.setColumns(10);
+		
+		lblNewLabel_1 = new JLabel("Tipo de juego");
+		add(lblNewLabel_1);
+		
+		
+		JList listajuegos;
+		String juego[] = { "Cartas", "Mesa", "Ping pong"};
+		listajuegos = new JList( juego );
 
+		list = new JList(juego);
+		
+	
+		lblNewLabel = new JLabel("El nombre del jugador 2 es:");
+		add(lblNewLabel);
+		
+		textField_1 = new JTextField();
+		add(textField_1);
+		textField_1.setColumns(10);
+		
+		
+		
+		btnNewButton = new JButton("Comenzar juego");
+		add(btnNewButton);
 	}
+	
 
+
+	public static String getConvertir() {
+		return CONVERTIR;
+	}
 }
