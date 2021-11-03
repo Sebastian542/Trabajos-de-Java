48eee10a95c299294d20574ca03d097f5be4baf0
Juan Sebastian Muñoz Peñuela <juansebas_2001@hotmail.com>
2021-11-03 12:59:07 -0500

    Agrego panel de ahorcado.

---
+++ EcorpJuegos/src/co/edu/unbosque/view/JuegoAhorcado.java
@@ -0,0 +1,14 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JPanel;
+
+public class JuegoAhorcado extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public JuegoAhorcado() {
+
+	}
+
+}
