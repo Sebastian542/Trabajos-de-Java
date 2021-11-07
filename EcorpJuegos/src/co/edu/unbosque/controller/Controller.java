63fe62ff673ac48fafb6ad8ee9f37357a7821d71
NicolasCamachoL <84592597+NicolasCamachoL@users.noreply.github.com>
2021-11-07 13:12:13 -0500

    Se muestran los paneles 1 2 y 3 por separado

--- EcorpJuegos/src/co/edu/unbosque/controller/Controller.java
+++ EcorpJuegos/src/co/edu/unbosque/controller/Controller.java
@@ -1,13 +1,9 @@
 package co.edu.unbosque.controller;
 
-
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import co.edu.unbosque.model.ClaseX;
-import co.edu.unbosque.view.Panel1;
-import co.edu.unbosque.view.Panel2;
 import co.edu.unbosque.view.View;
 
 public class Controller implements ActionListener{
@@ -15,46 +11,27 @@ public class Controller implements ActionListener{
 
 	private ClaseX m;
 	private View gui;
-	private Panel1 pan1;
 	
 	public Controller() {
 		m = new ClaseX();
-		
-		pan1 = new Panel1(null);
-		pan1.getOkButton().addActionListener(null);
-		pan1.getCancelButton().addActionListener(null);
-		//p2 = new Panel2(this);
-		//p2.setVisible(true);	
-		
-		
+
 		gui = new View(this);
 		gui.setVisible(true);
-		
-			
-		
-		
+
 		///funcionar();
 		
 	}
 	
 	public void funcionar() {
-		
-	
 
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent evento) {
 		if(evento.getActionCommand().equals(gui.getP4().JUGADORES)) {
-			pan1.setVisible(true);
+			gui.getP1().setVisible(true);
 			gui.getP4().setVisible(false);
 			gui.setVisible(false);
-			
-			if(evento.getActionCommand().equals(pan1.CANCEL)) {
-				pan1.setVisible(false);
-				gui.getP4().setVisible(true);
-				gui.setVisible(true);
-			}
 		}
 		
 		if(evento.getActionCommand().equals(gui.getP4().JUEGOS)) {
