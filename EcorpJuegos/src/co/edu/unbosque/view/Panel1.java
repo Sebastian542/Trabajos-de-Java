5893d7a58da35b19313ae62cb83d573887772a1d
Juan Sebastian Muñoz Peñuela <juansebas_2001@hotmail.com>
2021-11-06 22:15:37 -0500

    Panel1

--- EcorpJuegos/src/co/edu/unbosque/view/Panel1.java
+++ EcorpJuegos/src/co/edu/unbosque/view/Panel1.java
@@ -1,21 +1,8 @@
 package co.edu.unbosque.view;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import java.awt.BorderLayout;
-import javax.swing.JButton;
-import java.awt.GridLayout;
-import java.awt.GridBagLayout;
-import javax.swing.JTextField;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.TextField;
-import javax.swing.BoxLayout;
-import javax.swing.JList;
-import java.awt.Color;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Panel1 extends JPanel {
 	
