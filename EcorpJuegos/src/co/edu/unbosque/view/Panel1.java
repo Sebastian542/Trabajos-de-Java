7f240123f263731fa95de2eb1d67715831f91db5
bryan <bryan@DESKTOP-B3NBQ08>
2021-11-06 22:33:47 -0600

    Panel 1

--- EcorpJuegos/src/co/edu/unbosque/view/Panel1.java
+++ EcorpJuegos/src/co/edu/unbosque/view/Panel1.java
@@ -1,18 +1,48 @@
 package co.edu.unbosque.view;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class Panel1 extends JPanel {
+import co.edu.unbosque.controller.Controller;
+
+
+
+public class Panel1 extends  JDialog {
 	
-
+	private final JPanel contentPanel = new JPanel();
+	
+	
 	public Panel1(Controller control) {
-
 		
+	
+		
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
 	}
 	
 }
