7cae24aaf6acb85cf3ac07bfcc08509f5b177104
Juan Sebastian Muñoz Peñuela <juansebas_2001@hotmail.com>
2021-11-20 21:20:45 -0500

    Avances Modulo 2.

--- ECORPTIENDAGENERICA/src/co/edu/unbosque/controller/Controller.java
+++ ECORPTIENDAGENERICA/src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,87 @@
 package co.edu.unbosque.controller;
 
-public class Controller {
 
+
+import java.awt.event.ActionEvent;
+
+import co.edu.unbosque.model.Mundo;
+
+import co.edu.unbosque.view.View;
+
+public class Controller implements ActionListener {
+
+	//Atributos que vienen del Modelo (mundo) 
+	private Mundo bd;
+	
+	//Atributos que vienen de Vista - GUI
+	private View gui;
+	
+	
+	public Controller() {
+		
+		//aqui van los constructores tanto de Mundo, como de Vista
+		
+		bd = new Mundo();
+		//se Modifica contructor de InterfazGUI para que reciba como parámetro la instancia de Controlador
+		gui = new View(this);
+		gui.setVisible(true); //hacer visible la ventana principal
+		
+	}
+	
+	public void actionPerformed(ActionEvent evento) {
+		// TODO Auto-generated method stub
+		String resultado;
+		
+		
+		
+		if (evento.getActionCommand().equals(gui.getPanelEntrada().ESCRIBIR)) {
+			resultado = bd.getBD().escribirArchivoBinario();
+			gui.escribirMensaje(resultado);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		
+		if(evento.getActionCommand().equals(gui.getPanelEntrada().LEER)) {
+			//haga algo de por Dios!!
+			bd.getBD().leerArchivoBinario();
+			mostrarCifras();
+		}
+		
+		if(evento.getActionCommand().equals(gui.getPanelEntrada().ESCRIBIR_REG)) {
+			gui.escribirMensaje(bd.getBD().escribirRegistro());
+		}
+		
+		if(evento.getActionCommand().equals(gui.getPanelEntrada().LEER_REG)) {
+			bd.getBD().leerRegistro();
+			mostrarRegistros();
+			
+		}
+	}
+	
+	private void mostrarCifras() {
+		for (int i= 0 ; i<bd.getBD().getREGISTROS() ; i++) {
+			gui.getPanelResultados().getTxtMonto().append(String.valueOf(bd.getBD().getNumeros()[i])+"\n");
+			gui.getPanelResultados().getTxtValores().append(String.valueOf(bd.getBD().getValores()[i])+"\n");
+		}	
+	}
+	
+	private void mostrarRegistros() {
+		for (int i= 0 ; i<3 ; i++) {
+			gui.getPanelRegistro().getTxtEdad().append(String.valueOf(bd.getBD().getDatos()[i].getEdad())+"\n");
+			gui.getPanelRegistro().getTxtNombre().append(String.valueOf(bd.getBD().getDatos()[i].getNombre())+"\n");
+			gui.getPanelRegistro().getTxtSalario().append(String.valueOf(bd.getBD().getDatos()[i].getSalario())+"\n");
+		}	
+	}
+  */	
+	}
 }
---
+++ ECORPTIENDAGENERICA/src/co/edu/unbosque/model/Mundo.java
@@ -0,0 +1,44 @@
+package co.edu.unbosque.model;
+
+
+public class Mundo {
+	
+	private JuegosFile GF;//se asocia a Mundo para que éste pueda interactuar con la data de Persistencia.
+	private PartidasFile PF;
+	private JugadorFile JF;
+	
+	
+
+	public Mundo() {
+		// TODO Auto-generated constructor stub
+		GF = new JuegosFile();
+	
+	}
+	
+	public JuegosFile getGF() {
+		return GF;
+	}
+
+	public void setGF(JuegosFile gF) {
+		GF = gF;
+	}
+
+	public PartidasFile getPF() {
+		return PF;
+	}
+
+	public void setPF(PartidasFile pF) {
+		PF = pF;
+	}
+
+	public JugadorFile getJF() {
+		return JF;
+	}
+
+	public void setJF(JugadorFile jF) {
+		JF = jF;
+	}
+	
+	
+
+}
