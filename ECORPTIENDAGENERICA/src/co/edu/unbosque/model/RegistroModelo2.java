64e36e103ab8a4dfb98bc90dee7aef8a3b423dc4
Juan Sebastian Muñoz Peñuela <juansebas_2001@hotmail.com>
2021-11-22 13:44:17 -0500

    Se arreglan los parametros del constructor en RegistroModelo2

--- ECORPTIENDAGENERICA/src/co/edu/unbosque/model/RegistroModelo2.java
+++ ECORPTIENDAGENERICA/src/co/edu/unbosque/model/RegistroModelo2.java
@@ -9,37 +9,60 @@ public class RegistroModelo2 implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private int cedula;
 	private String nombre;
-	private int edad;
-	private double salario;
+	private String direccion;
+	private int telefono;
+	private String correo;
 	
-	public RegistroModelo2(){
+	public RegistroModelo2(int cedula, String nombre, String direccion, int telefono, String correo) {
+	
+		this.cedula = cedula;
 		this.nombre = nombre;
-		this.edad = edad;
-		this.salario = salario;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.correo = correo;
 	}
-	
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	public int getEdad() {
-		return edad;
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
 	}
 	
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
 	
-	public double getSalario() {
-		return salario;
-	}
-	
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
 }
