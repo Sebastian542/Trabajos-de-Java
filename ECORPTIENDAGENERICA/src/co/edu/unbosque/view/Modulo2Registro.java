ab22aa3d966366f18289ebe19e17c4e6a351e70c
Juan Sebastian Muñoz Peñuela <juansebas_2001@hotmail.com>
2021-11-30 22:59:47 -0500

    Solucion Modulo2

--- ECORPTIENDAGENERICA/src/co/edu/unbosque/view/Modulo2Registro.java
+++ ECORPTIENDAGENERICA/src/co/edu/unbosque/view/Modulo2Registro.java
@@ -9,6 +9,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import co.edu.unbosque.controller.Controller;
+
 import java.awt.Color;
 import java.awt.Font;
 
